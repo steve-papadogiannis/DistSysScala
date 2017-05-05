1001fe2d71ab181d2fba2bf1c0055e30a6f4cc5c
stefanos <stefanos>
2017-05-05 20:20:55 +0300

    Adding constants

---
+++ src/main/scala/ApplicationConstants.scala
@@ -0,0 +1,19 @@
+/**
+  * Created by stefanos on 5/5/17.
+  */
+object ApplicationConstants {
+  val DIRECTIONS_API_KEY = "AIzaSyDS8v7RI9Pzx75PwpmUJGLMDR_98kHGzP4"
+  val LOCALHOST = "127.0.0.1"
+  val ATHENS = "ATHENS"
+  val JAMAICA = "JAMAICA"
+  val HAVANA = "HAVANA"
+  val SAO_PAOLO = "SAO_PAOLO"
+  val MOSCOW = "MOSCOW"
+  val ANDROID = "ANDROID"
+  val ATHENS_PORT = 5555
+  val JAMAICA_PORT = 5556
+  val HAVANA_PORT = 5557
+  val SAO_PAOLO_PORT = 5558
+  val MOSCOW_PORT = 5559
+  val ANDROID_PORT = 9999
+}
