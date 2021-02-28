d46d90269c64fabbe829b84a43da1b0c826e823b
Stefanos Papadogiannis <steve.papadogiannis1992@gmail.com>
2021-02-28 19:10:30 +0200

    Refactoring supervisor.

--- src/main/scala/gr/papadogiannis/stefanos/supervisors/Supervisor.scala
+++ src/main/scala/gr/papadogiannis/stefanos/supervisors/Supervisor.scala
@@ -6,22 +6,18 @@ import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import gr.papadogiannis.stefanos.servers.Server
 
 object Supervisor {
-
   def props(): Props = Props(new Supervisor)
-
 }
 
-class Supervisor
-  extends Actor
-    with ActorLogging {
+class Supervisor extends Actor with ActorLogging {
+
+  val serverName = "server"
+
+  var server: ActorRef = _
 
   override def preStart(): Unit = log.info("Supervisor started")
 
   override def postStop(): Unit = log.info("Supervisor stopped")
-
-  var server: ActorRef = _
-
-  val serverName = "server"
 
   override def receive: Receive = {
     case CreateInfrastructure =>
