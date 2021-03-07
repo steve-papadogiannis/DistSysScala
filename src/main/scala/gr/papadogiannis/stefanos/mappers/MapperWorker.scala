4033d6fa4ef613930c7237e5e290d4cef90a555b
Stefanos Papadogiannis <steve.papadogiannis1992@gmail.com>
2021-03-07 19:01:52 +0200

    Refactoring mapper worker [ci skip].

--- src/main/scala/gr/papadogiannis/stefanos/mappers/MapperWorker.scala
+++ src/main/scala/gr/papadogiannis/stefanos/mappers/MapperWorker.scala
@@ -1,8 +1,9 @@
 package gr.papadogiannis.stefanos.mappers
 
-import org.mongodb.scala.{ConnectionString, MongoClient, MongoClientSettings, Observable}
+import org.mongodb.scala.{ConnectionString, MongoClient, MongoClientSettings, MongoDatabase, Observable}
 import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
 import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
+import org.bson.codecs.configuration.CodecRegistry
 import akka.actor.{Actor, ActorLogging, Props}
 import org.mongodb.scala.bson.codecs.Macros._
 import gr.papadogiannis.stefanos.models._
@@ -66,7 +67,85 @@ class MapperWorker(mapperId: String) extends Actor with ActorLogging {
   }
 
   def map(startGeoPoint: GeoPoint, endGeoPoint: GeoPoint): Observable[Seq[Map[GeoPointPair, DirectionsResult]]] = {
-    val codecs = fromRegistries(
+    val codecRegistries = getCodecRegistries
+    val mongoClientSettings = getMongoClientSettings(codecRegistries)
+    val mongoClient = getMongoClient(mongoClientSettings)
+    val database = getMongoDatabase(mongoClient)
+    val collection = getMongoCollection(database)
+    val mapperHash: Long = getMapperHash
+    collection
+      .find[DirectionsResultWrapper]()
+      .filter(x => {
+        val geoPointsHashMod4: Long = getGeoPointsHash(x)
+        mapperHash == geoPointsHashMod4
+      })
+      .map(x => {
+        val map = Map.empty[GeoPointPair, DirectionsResult]
+        val isStartLatitudeNearIssuedStartLatitude = Math.abs(startGeoPoint.latitude - x.startPoint.latitude) < 0.0001
+        val isStartLongitudeNearIssuedStartLongitude = Math.abs(startGeoPoint.longitude - x.startPoint.longitude) < 0.0001
+        val isEndLatitudeNearIssuedEndLatitude = Math.abs(endGeoPoint.latitude - x.endPoint.latitude) < 0.0001
+        val isEndLongitudeNearIssuedEndLongitude = Math.abs(endGeoPoint.longitude - x.endPoint.longitude) < 0.0001
+        if (isStartLatitudeNearIssuedStartLatitude &&
+          isStartLongitudeNearIssuedStartLongitude &&
+          isEndLatitudeNearIssuedEndLatitude &&
+          isEndLongitudeNearIssuedEndLongitude) {
+          val geoPointPair = new GeoPointPair(
+            GeoPoint(
+              roundTo2Decimals(x.startPoint.latitude),
+              roundTo2Decimals(x.startPoint.longitude)),
+            GeoPoint(
+              roundTo2Decimals(x.endPoint.latitude),
+              roundTo2Decimals(x.endPoint.longitude)))
+          map + (geoPointPair -> x.directionsResult)
+        }
+        map
+      })
+      .filter(x => x.nonEmpty)
+      .collect()
+  }
+
+  private def getGeoPointsHash(x: DirectionsResultWrapper) = {
+    val geoPointsHash = calculateHash(
+      x.startPoint.latitude.toString +
+        x.startPoint.longitude.toString +
+        x.endPoint.latitude.toString +
+        x.endPoint.longitude.toString)
+    val geoPointsHashModFour = geoPointsHash % 4
+    if (geoPointsHashModFour < 0) -geoPointsHashModFour else geoPointsHashModFour
+  }
+
+  private def getMapperHash = {
+    val mapperHash = calculateHash(mapperId)
+    val mapperHashModFour = mapperHash % 4
+    if (mapperHashModFour < 0) -mapperHashModFour else mapperHashModFour
+  }
+
+  private def getMongoCollection(database: MongoDatabase) = {
+    database.getCollection("directions")
+  }
+
+  private def getMongoDatabase(mongoClient: MongoClient) = {
+    mongoClient.getDatabase("dist-sys")
+  }
+
+  private def getMongoClient(mongoClientSettings: MongoClientSettings) = {
+    MongoClient(mongoClientSettings)
+  }
+
+  private def getMongoClientSettings(codecRegistries: CodecRegistry) = {
+    MongoClientSettings
+      .builder()
+      .applyConnectionString(new ConnectionString(getMongoConnectionString))
+      .codecRegistry(codecRegistries)
+      .build()
+  }
+
+  private def getMongoConnectionString = {
+    "mongodb://root:example@localhost:27017"
+  }
+
+  private def getCodecRegistries = {
+    fromRegistries(
       fromProviders(
         classOf[DirectionsResultWrapper],
         classOf[GeoPoint],
@@ -79,44 +158,6 @@ class MapperWorker(mapperId: String) extends Actor with ActorLogging {
         classOf[EncodedPolyline]
       ),
       DEFAULT_CODEC_REGISTRY)
-    val mongoClientSettings = MongoClientSettings
-      .builder()
-      .applyConnectionString(new ConnectionString("mongodb://root:example@localhost:27017"))
-      .codecRegistry(codecs)
-      .build()
-    val mongoClient = MongoClient(mongoClientSettings)
-    val database = mongoClient.getDatabase("dist-sys")
-    val collection = database.getCollection("directions")
-    val ipPortHash = calculateHash("127.0.0.1" + 5555)
-    val ipPortHashMod4 = if (ipPortHash % 4 < 0)
-      -(ipPortHash % 4)
-    else
-      ipPortHash % 4
-    collection.find[DirectionsResultWrapper]().filter(x => {
-        val geoPointsHash = calculateHash(x.startPoint.latitude.toString + x.startPoint.longitude.toString +
-          x.endPoint.latitude.toString + x.endPoint.longitude.toString)
-        val geoPointsHashMod4 = if (geoPointsHash % 4 < 0)
-          -(geoPointsHash % 4)
-        else
-          geoPointsHash % 4
-        ipPortHashMod4 == geoPointsHashMod4
-    }).map(x => {
-        val map = Map.empty[GeoPointPair, DirectionsResult]
-        val isStartLatitudeNearIssuedStartLatitude = Math.abs(startGeoPoint.latitude - x.startPoint.latitude) < 0.0001
-        val isStartLongitudeNearIssuedStartLongitude = Math.abs(startGeoPoint.longitude - x.startPoint.longitude) < 0.0001
-        val isEndLatitudeNearIssuedEndLatitude = Math.abs(endGeoPoint.latitude - x.endPoint.latitude) < 0.0001
-        val isEndLongitudeNearIssuedEndLongitude = Math.abs(endGeoPoint.longitude - x.endPoint.longitude) < 0.0001
-        if (isStartLatitudeNearIssuedStartLatitude &&
-          isStartLongitudeNearIssuedStartLongitude &&
-          isEndLatitudeNearIssuedEndLatitude &&
-          isEndLongitudeNearIssuedEndLongitude) {
-          val geoPointPair = new GeoPointPair(new GeoPoint(roundTo2Decimals(x.startPoint.latitude),
-            roundTo2Decimals(x.startPoint.longitude)), new GeoPoint(roundTo2Decimals(x.endPoint.latitude),
-            roundTo2Decimals(x.endPoint.longitude)))
-          map + (geoPointPair -> x.directionsResult)
-        }
-        map
-    }).filter(x => x.nonEmpty).collect()
   }
 
 }
