7cf38a031a6ec410ee85e8df938ced6010e2cd96
Stefanos Papadogiannis <steve.papadogiannis1992@gmail.com>
2021-03-07 13:51:10 +0200

    Adding logging in request handler [ci skip].

--- src/main/scala/gr/papadogiannis/stefanos/handlers/RequestHandler.scala
+++ src/main/scala/gr/papadogiannis/stefanos/handlers/RequestHandler.scala
@@ -1,22 +1,27 @@
 package gr.papadogiannis.stefanos.handlers
 
 import gr.papadogiannis.stefanos.models.{CalculateDirections, FinalResponse, Handle}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import gr.papadogiannis.stefanos.Main.supervisor
 import com.google.maps.model.DirectionsResult
-import akka.actor.{Actor, ActorRef, Props}
 
 object RequestHandler {
   def props(): Props = Props(new RequestHandler())
 }
 
-class RequestHandler() extends Actor {
+class RequestHandler() extends Actor with ActorLogging {
 
   var complete: DirectionsResult => Unit = _
 
   var requester: ActorRef = _
 
+  override def preStart(): Unit = log.info("RequestHandler actor started")
+
+  override def postStop(): Unit = log.info("RequestHandler actor stopped")
+
   override def receive: Receive = {
-    case Handle(requestId, startLat, startLong, endLat, endLong, f) =>
+    case message@Handle(requestId, startLat, startLong, endLat, endLong, f) =>
+      log.info(message.toString)
       requester = sender()
       complete = f
       supervisor ! CalculateDirections(requestId, startLat, startLong, endLat, endLong)
