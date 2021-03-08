2a99720442b97b0d1fccc388a022c1dcfa896d12
Stefanos Papadogiannis <steve.papadogiannis1992@gmail.com>
2021-03-08 22:27:10 +0200

    Refactoring google directions api actor [ci skip].

--- src/main/scala/gr/papadogiannis/stefanos/integrations/GoogleDirectionsAPIActor.scala
+++ src/main/scala/gr/papadogiannis/stefanos/integrations/GoogleDirectionsAPIActor.scala
@@ -1,10 +1,10 @@
 package gr.papadogiannis.stefanos.integrations
 
 import akka.actor.{Actor, ActorLogging, Props}
-import com.google.maps.model.LatLng
 import com.google.maps.{DirectionsApi, GeoApiContext}
 import gr.papadogiannis.stefanos.constants.ApplicationConstants.RECEIVED_MESSAGE_PATTERN
 import gr.papadogiannis.stefanos.messages.GetDirections
+import gr.papadogiannis.stefanos.models.{DirectionsLeg, DirectionsResult, DirectionsRoute, DirectionsStep, Duration, EncodedPolyline, LatLng}
 
 object GoogleDirectionsAPIActor {
   def props(): Props = Props(new GoogleDirectionsAPIActor)
@@ -22,10 +22,17 @@ class GoogleDirectionsAPIActor extends Actor with ActorLogging {
       val geoApiContext = new GeoApiContext
       geoApiContext.setApiKey("")
       val maybeResult = try {
-        val directionsResult = DirectionsApi
+        val googleDirectionsResult = DirectionsApi
           .newRequest(geoApiContext)
-          .origin(new LatLng(geoPointPair.startGeoPoint.latitude, geoPointPair.startGeoPoint.longitude))
-          .destination(new LatLng(geoPointPair.endGeoPoint.latitude, geoPointPair.endGeoPoint.longitude)).await
+          .origin(
+            new com.google.maps.model.LatLng(
+              geoPointPair.startGeoPoint.latitude,
+              geoPointPair.startGeoPoint.longitude))
+          .destination(
+            new com.google.maps.model.LatLng(
+              geoPointPair.endGeoPoint.latitude,
+              geoPointPair.endGeoPoint.longitude)).await
+        val directionsResult = convert(googleDirectionsResult)
         Some(directionsResult)
       } catch {
         case exception: Exception =>
@@ -34,5 +41,22 @@ class GoogleDirectionsAPIActor extends Actor with ActorLogging {
       }
       sender() ! maybeResult
   }
+
+  def convert(googleDirectionsResult: com.google.maps.model.DirectionsResult): DirectionsResult = {
+    val routes = googleDirectionsResult.routes.toStream.map(route => {
+      val legs = route.legs.toStream.map(leg => {
+        val steps = leg.steps.toStream.map(step => {
+          DirectionsStep(
+            LatLng(step.startLocation.lat, step.startLocation.lng),
+            LatLng(step.endLocation.lat, step.endLocation.lng),
+            EncodedPolyline(step.polyline.getEncodedPath))
+        }).toList
+        DirectionsLeg(steps, Duration(leg.duration.inSeconds))
+      }).toList
+      DirectionsRoute(legs)
+    }).toList
+    DirectionsResult(routes)
+  }
+
 
 }
